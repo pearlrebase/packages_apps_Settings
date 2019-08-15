539a0e8a77e3463d3ec5141dccddb8c97ef98fd7
golden-guy <golden-guygolden-guy@users.noreply.github.com>
2019-08-15 06:20:16 +0000

    Settings: Bring back Uptime to Battery Page

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -53,6 +53,7 @@ import com.android.settings.Utils;
 import com.android.settings.applications.LayoutPreference;
 import com.android.settings.core.SubSettingLauncher;
 import com.android.settings.dashboard.SummaryLoader;
+import com.android.settings.fuelgauge.UpTimePreferenceController;
 import com.android.settings.display.BatteryPercentagePreferenceController;
 import com.android.settings.fuelgauge.anomaly.Anomaly;
 import com.android.settings.fuelgauge.anomaly.AnomalyDetectionPolicy;
@@ -294,6 +295,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                 BatteryTipListener */);
         controllers.add(mBatteryTipPreferenceController);
         controllers.add(new BatteryPercentagePreferenceController(context));
+        controllers.add(new UpTimePreferenceController(context));
         return controllers;
     }
 
