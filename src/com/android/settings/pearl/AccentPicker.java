SuperiorOS/android_packages_apps_Settings

Use unique package name for themes[1/3]
With treble vendor its possible that /vendor may already contain packages
having similar package name. Rename the arrow theme packages to ensure
that desired theme package gets loaded.

Change-Id: I23f74c72629966597a4d7dc6d9674a89808acdbd
Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

src/com/android/settings/superior/AccentPicker.java
@@ -410,7 +410,7 @@ public class AccentPicker extends InstrumentedDialogFragment implements OnClickL
     private boolean isUsingDarkTheme() {
         OverlayInfo themeInfo = null;
         try {
-            themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.dark",
+            themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.dark.superior",
                     UserHandle.USER_CURRENT);
         } catch (RemoteException e) {
             e.printStackTrace();
@@ -422,7 +422,7 @@ public class AccentPicker extends InstrumentedDialogFragment implements OnClickL
     private boolean isUsingBlackTheme() {
         OverlayInfo themeInfo = null;
         try {
-            themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.black",
+            themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.black.superior",
                     UserHandle.USER_CURRENT);
         } catch (RemoteException e) {
             e.printStackTrace();

pearlrebase/packages_apps_Settings

src/com/android/settings/pearl/AccentPicker.java
@@ -410,7 +410,7 @@ public class AccentPicker extends InstrumentedDialogFragment implements OnClickL
     private boolean isUsingDarkTheme() {
         OverlayInfo themeInfo = null;
         try {
-            themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.dark",
+            themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.dark.pearl",
                     UserHandle.USER_CURRENT);
         } catch (RemoteException e) {
             e.printStackTrace();
@@ -422,7 +422,7 @@ public class AccentPicker extends InstrumentedDialogFragment implements OnClickL
     private boolean isUsingBlackTheme() {
         OverlayInfo themeInfo = null;
         try {
-            themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.black",
+            themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.black.pearl",
                     UserHandle.USER_CURRENT);
         } catch (RemoteException e) {
             e.printStackTrace();