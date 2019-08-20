2a592d9c95ef694a5d2abdd3e57229351550903d
Mazda-- <Mazda--@users.noreply.github.com>
2019-08-20 20:26:58 +0000

    Battery temp: Ensure preference is updated
    I observed a rare situation where the preference isn't updated and it
    gets stuck on the last reading. Switching between accounts (multiuser),
    I was able to spot the jank. This commit ensures that this doesn't
    happen as it will be updated the same time other preferences are updated.
    
    Change-Id: Iaa9e9cb757ce4139a7492c3d9405212ff26168b7

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -225,6 +225,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         restartBatteryInfoLoader();
         mBatteryTipPreferenceController.restoreInstanceState(icicle);
         updateBatteryTipFlag(icicle);
+        updateBatteryTempPreference();
     }
 
     @Override
@@ -237,15 +238,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                         .launch();
             return true;
         } else if (KEY_BATTERY_TEMP.equals(preference.getKey())) {
-            if (batteryTemp) {
-                mBatteryTemp.setSubtitle(
-                    com.android.internal.util.pearl.PearlUtils.batteryTemperature(getContext(), false));
-                batteryTemp = false;
-            } else {
-                mBatteryTemp.setSubtitle(
-                    com.android.internal.util.pearl.PearlUtils.batteryTemperature(getContext(), true));
-                batteryTemp = true;
-            }
+            updateBatteryTempPreference();
         }
         return super.onPreferenceTreeClick(preference);
     }
@@ -345,10 +338,9 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
         updateLastFullChargePreference();
+        updateBatteryTempPreference();
         mScreenUsagePref.setSubtitle(StringUtil.formatElapsedTime(getContext(),
                 mBatteryUtils.calculateScreenUsageTime(mStatsHelper), false));
-        mBatteryTemp.setSubtitle(
-                com.android.internal.util.pearl.PearlUtils.batteryTemperature(getContext(), batteryTemp));
     }
 
     @VisibleForTesting
@@ -381,6 +373,19 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
             mLastFullChargePref.setSubtitle(
                     StringUtil.formatRelativeTime(getContext(), lastFullChargeTime,
                             false /* withSeconds */));
+        }
+    }
+
+    @VisibleForTesting
+    void updateBatteryTempPreference() {
+        if (batteryTemp) {
+            mBatteryTemp.setSubtitle(
+                com.android.internal.util.pearl.PearlUtils.batteryTemperature(getContext(), false));
+            batteryTemp = false;
+        } else {
+            mBatteryTemp.setSubtitle(
+                com.android.internal.util.pearl.PearlUtils.batteryTemperature(getContext(), true));
+            batteryTemp = true;
         }
     }
 
